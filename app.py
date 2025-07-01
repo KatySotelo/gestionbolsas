5ab757f5f18f56ab70169c643049c44284906e4f
Katy Sotelo <katy.sotelo.d@gmail.com>
2025-06-30 21:23:45 -0400

    Agregué login con usuario y contraseña

--- app.py
+++ app.py
@@ -4,11 +4,68 @@ Created on Mon Jun 30 17:56:43 2025
 
 @author: Katy
 """
-from flask import Flask, render_template, request, redirect
 import sqlite3
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import os
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY", "clave-supersecreta")
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+# Usuario único definido manualmente
+USUARIO_VALIDO = {
+    "username": "Katy",
+    "password": "Katysotelo1"  # Puedes cambiarlo
+}
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username == USUARIO_VALIDO["username"] and password == USUARIO_VALIDO["password"]:
+            user = User(username)
+            login_user(user)
+            return redirect(url_for("home"))
+        else:
+            flash("Usuario o contraseña incorrectos", "error")
+    return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+@app.route("/")
+@login_required
+def home():
+    return render_template("home.html")
+
+@app.route("/ingreso")
+@login_required
+def ingreso():
+    return render_template("ingreso.html")
+
+@app.route("/rebaja")
+@login_required
+def rebaja():
+    return render_template("rebaja.html")
+
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+@login_manager.user_loader
+def load_user(user_id):
+    if user_id == "admin":
+        return User(user_id)
+    return None
 
 # Inicializa base de datos
 def init_db():
